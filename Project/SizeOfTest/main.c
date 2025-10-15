50e5f3bc5b309a077ca451a2c4c4b4551e079392
sauronpi <sauronpi@outlook.com>
2025-10-15 20:09:02 +0800

    style: SizeOfTest printf format

--- Project/SizeOfTest/main.c
+++ Project/SizeOfTest/main.c
@@ -2,13 +2,13 @@
 
 int main(int argc, const char *argv[]) {
     void *point = NULL;
-    printf("short int size %lu byte\n", sizeof(short int));
-    printf("int size %lu byte\n", sizeof(int));
-    printf("long int size %lu byte\n", sizeof(long int));
-    printf("long long size %lu byte\n", sizeof(long long));
-    printf("float %lu byte\n", sizeof(float));
-    printf("double %lu byte\n", sizeof(double));
-    printf("bool size %lu byte\n", sizeof(bool));
-    printf("char size %lu byte\n", sizeof(char));
-    printf("void point %lu byte\n", sizeof(point));
+    printf("short int size\t%lu byte\n", sizeof(short int));
+    printf("int size\t%lu byte\n", sizeof(int));
+    printf("long int size\t%lu byte\n", sizeof(long int));
+    printf("long long size\t%lu byte\n", sizeof(long long));
+    printf("float\t\t%lu byte\n", sizeof(float));
+    printf("double\t\t%lu byte\n", sizeof(double));
+    printf("bool size\t%lu byte\n", sizeof(bool));
+    printf("char size\t%lu byte\n", sizeof(char));
+    printf("void point\t%lu byte\n", sizeof(point));
 }
