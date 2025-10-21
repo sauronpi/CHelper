1e4b6fb904a8ebcfbec9da4d1b3c5d3ad3579815
SauronPi <sauronpi@outlook.com>
2025-10-21 11:12:34 +0800

    style: printf format

--- Project/SizeOfTest/main.c
+++ Project/SizeOfTest/main.c
@@ -1,14 +1,14 @@
-#include "Foundation.h"
+#include <Foundation.h>
 
 int main(int argc, const char *argv[]) {
     void *point = NULL;
-    printf("short int size\t%lu byte\n", sizeof(short int));
-    printf("int size\t%lu byte\n", sizeof(int));
-    printf("long int size\t%lu byte\n", sizeof(long int));
-    printf("long long size\t%lu byte\n", sizeof(long long));
-    printf("float\t\t%lu byte\n", sizeof(float));
-    printf("double\t\t%lu byte\n", sizeof(double));
-    printf("bool size\t%lu byte\n", sizeof(bool));
-    printf("char size\t%lu byte\n", sizeof(char));
-    printf("void point\t%lu byte\n", sizeof(point));
+    printf("short int size\t%llu byte\n", sizeof(short int));
+    printf("int size\t%llu byte\n", sizeof(int));
+    printf("long int size\t%llu byte\n", sizeof(long int));
+    printf("long long size\t%llu byte\n", sizeof(long long));
+    printf("float\t\t%llu byte\n", sizeof(float));
+    printf("double\t\t%llu byte\n", sizeof(double));
+    printf("bool size\t%llu byte\n", sizeof(bool));
+    printf("char size\t%llu byte\n", sizeof(char));
+    printf("void point\t%llu byte\n", sizeof(point));
 }
